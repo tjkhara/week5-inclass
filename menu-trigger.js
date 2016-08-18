05b300c89107d8182e10287e8522491be9aac76d
Tajeshwar Khara <tkhara@gmail.com>
2016-08-17 18:02:40 -0700

    Commit 2
    Transitioned through the stacked menu and then the hamburger menu.

---
+++ menu-trigger.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+// Responsive Menu - Trigger to show/hide menu
+
+ function toggle_menu(id) {
+       var element = document.getElementById(id);
+       if(element.style.display == 'block')
+          element.style.display = 'none';
+       else
+          element.style.display = 'block';
+    }
